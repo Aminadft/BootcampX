0dd8cadbfaacbf926ec83767c683c37a1f214438
Amina Malik <amina.malik@telus.com>
2022-12-15 21:07:28 -0500

    merged js and sql

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+const cohortsName = process.argv[2];
+
+const val = [`%${cohortsName}%`];
+
+const queryCommand = `
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
+FROM teachers
+JOIN assistance_requests
+ON assistance_requests.teacher_id = teachers.id
+JOIN students
+ON students.id = assistance_requests.student_id
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+GROUP BY cohorts.name, teachers.name
+ORDER BY teachers.name;
+`;
+
+pool.query(queryCommand, val)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
